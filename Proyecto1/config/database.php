772365d5f88d34c47ba7116e1af937bb43c67387
JoseUrbina <joseantonioug-09@hotmail.com>
2018-07-04 16:08:16 -0600

    Create database configuration file

---
+++ Proyecto1/config/database.php
@@ -0,0 +1,19 @@
+<?php
+	use Illuminate\Database\Capsule\Manager as Capsule;
+
+	$capsule = new Capsule;
+
+	$capsule->addConnection([
+		'driver' 	=> 'mysql',
+		'host' 	 	=> 'localhost',
+		'database'	=> 'basic',
+		'username'	=> 'root',
+		'password'	=> 'root',
+		'charset'	=> 'utf8',
+		'collation'	=> 'utf8_unicode_ci',
+		'prefix' 	=> '' 
+	]);
+
+	// Dar vida a eloquent, usando la configuración previa
+	$capsule->bootEloquent();
+?>
